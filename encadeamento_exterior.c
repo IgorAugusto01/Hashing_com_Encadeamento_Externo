716d0d0e935f514016e17fa8841477492f405a65
IgorAugusto01 <augustoigor731@gmail.com>
2025-03-02 19:20:16 -0300

    código reduzido

--- encadeamento_exterior.c
+++ encadeamento_exterior.c
@@ -8,31 +8,6 @@
 #include <stdlib.h>
 #include "cliente.h"
 
-int buscaRecursiva(int end, int cod_cli, char *nome_arquivo_dados)
-{
-    if (end == -1)
-        return -1;
-    FILE *dados = fopen(nome_arquivo_dados, "rb");
-    fseek(dados, (end * sizeof(Cliente)), SEEK_SET);
-    Cliente *c = le_cliente(dados);
-    fclose(dados);
-    if (c->cod_cliente == cod_cli)
-    {
-        if (c->status == OCUPADO)
-        {
-            free(c);
-            return end;
-        }
-        end = c->prox;
-        free(c);
-        return buscaRecursiva(end, cod_cli, nome_arquivo_dados);
-    }
-    end = c->prox;
-    free(c);
-
-    return buscaRecursiva(end, cod_cli, nome_arquivo_dados);
-}
-
 int posicaoLivre(char *nome_arquivo_dados)
 {
     FILE *arqDados = fopen(nome_arquivo_dados, "rb");
@@ -52,118 +27,138 @@ int posicaoLivre(char *nome_arquivo_dados)
     return i;
 }
 
-int hash(int valor, int tam)
+int hash(int valor, char *nome_arquivo_hash)
 {
+    FILE *tab_hash = fopen(nome_arquivo_hash, "rb+");
+    ListaCompartimentos *lComp = le_compartimentos(nome_arquivo_hash);
+    int tam = lComp->qtd;
+    fclose(tab_hash);
     return valor % tam;
 }
 
 void cria_hash(char *nome_arquivo_hash, int tam)
 {
-    FILE* tab_hash = fopen(nome_arquivo_hash,"wb");
-   int i = 0;
-    while(i < tam)
+    FILE *tab_hash = fopen(nome_arquivo_hash, "wb");
+    int i = 0;
+    while (i < tam)
     {
-     fseek(tab_hash,(i*sizeof(CompartimentoHash)),SEEK_SET);
-     CompartimentoHash *comp = compartimento_hash(-1);
-     salva_compartimento(comp,tab_hash);
-     free(comp);
-     i++;
+        fseek(tab_hash, (i * sizeof(CompartimentoHash)), SEEK_SET);
+        CompartimentoHash *comp = compartimento_hash(-1);
+        salva_compartimento(comp, tab_hash);
+        free(comp);
+        i++;
     }
     fclose(tab_hash);
 }
 
 int busca(int cod_cli, char *nome_arquivo_hash, char *nome_arquivo_dados)
 {
+    FILE *arqDados = fopen(nome_arquivo_dados, "rb");
+    FILE *tab_hash = fopen(nome_arquivo_hash, "rb");
+    int end = hash(cod_cli, nome_arquivo_hash);
+    fseek(tab_hash, end * (sizeof(CompartimentoHash)), SEEK_SET);
+    fread(&end, sizeof(int), 1, tab_hash);
+    fclose(tab_hash);
 
-    ListaCompartimentos *lComp = le_compartimentos(nome_arquivo_hash);
-    int tam = lComp->qtd;
-    int h = hash(cod_cli, tam);
-    int end = lComp->lista[h]->prox;
-    free(lComp);
-    return (end == -1) ? -1 : buscaRecursiva(end, cod_cli, nome_arquivo_dados);
+    while (end != -1)
+    {
+        fseek(arqDados, (end * sizeof(Cliente)), SEEK_SET);
+        Cliente *c = le_cliente(arqDados);
+        if (c->cod_cliente == cod_cli)
+        {
+
+            if (c->status == OCUPADO)
+            {
+                free(c);
+                break;
+            }
+        }
+        end = c->prox;
+        free(c);
+    }
+    fclose(arqDados);
+    return end;
 }
 
 int insere(int cod_cli, char *nome_cli, char *nome_arquivo_hash, char *nome_arquivo_dados, int num_registros)
 {
     FILE *arqDados = fopen(nome_arquivo_dados, "rb+");
-
-    int end = busca(cod_cli, nome_arquivo_hash, nome_arquivo_dados);
-    if (end != -1)
+    Cliente *c = cliente(cod_cli, nome_cli, -1, OCUPADO);
+    ListaCompartimentos *compartimentos = le_compartimentos(nome_arquivo_hash);
+    int end = hash(cod_cli, nome_arquivo_hash);
+    if (compartimentos->lista[end]->prox == -1)
     {
-        fseek(arqDados, (end * sizeof(Cliente)), SEEK_SET);
-        Cliente *c = le_cliente(arqDados);
-        if (c->status == OCUPADO)
-        {
-            free(c);
-            fclose(arqDados);
-            return -1;
-        }
-        else
-        {
-            c->status = OCUPADO;
-            fseek(arqDados, (end * sizeof(Cliente)), SEEK_SET);
-            salva_cliente(c, arqDados);
-            free(c);
-            fclose(arqDados);
-            return end;
-        }
-    }
-
-    FILE *tab_hash = fopen(nome_arquivo_hash, "rb+");
-    ListaCompartimentos *lc = le_compartimentos(nome_arquivo_hash);
-    int pLivre = posicaoLivre(nome_arquivo_dados);
-    end = hash(cod_cli, lc->qtd);
-    free(lc);
-    fseek(tab_hash, (end * sizeof(CompartimentoHash)), SEEK_SET);
-    CompartimentoHash *comp = le_compartimento(tab_hash);
-    if (comp->prox == -1)
-    {
-        comp->prox = pLivre;
-        fseek(tab_hash, (end * sizeof(CompartimentoHash)), SEEK_SET);
-        salva_compartimento(comp, tab_hash);
-        free(comp);
-        fseek(arqDados, (pLivre * sizeof(Cliente)), SEEK_SET);
-        Cliente *c = cliente(cod_cli, nome_cli, -1, OCUPADO);
+        int livre = posicaoLivre(nome_arquivo_dados);
+        compartimentos->lista[end]->prox = livre;
+        fseek(arqDados, (livre * sizeof(Cliente)), SEEK_SET);
         salva_cliente(c, arqDados);
+        salva_compartimentos(nome_arquivo_hash, compartimentos);
+        free(c);
+        free(compartimentos);
+        end = livre;
+        fclose(arqDados);
+        return end;
+    }
+    else
+    {
+
+        Cliente *aux;
+        end = compartimentos->lista[end]->prox;
+        free(compartimentos);
+        int prox = end;
+        int atual = -1;
+        do
+        {
+     
+            fseek(arqDados, (prox * sizeof(Cliente)), SEEK_SET);
+            aux = le_cliente(arqDados);
+            if (aux->cod_cliente == cod_cli)
+            {
+                if (aux->status == LIBERADO)
+                {
+                    c->prox = aux->prox;
+                    fseek(arqDados, (prox * sizeof(Cliente)), SEEK_SET);
+                    salva_cliente(c, arqDados);
+                    fclose(arqDados);
+                    free(c);
+                    free(aux);
+                    return prox;
+                }
+                fclose(arqDados);
+                free(c);
+                free(aux);
+                return -1;
+            }
+            else
+            {
+                if (aux->status == LIBERADO)
+                {
+                    c->prox = aux->prox;
+                    fseek(arqDados, (prox * sizeof(Cliente)), SEEK_SET);
+                    salva_cliente(c, arqDados);
+                    fclose(arqDados);
+                    free(c);
+                    free(aux);
+                    return prox;
+                }
+            }
+            atual = prox;
+            prox = aux->prox;
+
+        } while (prox != -1);
+        int livre = posicaoLivre(nome_arquivo_dados);
+        aux->prox = livre;
+        fseek(arqDados, (atual * sizeof(Cliente)), SEEK_SET);
+        salva_cliente(aux, arqDados);
+        end = livre;
+        fseek(arqDados, (end * sizeof(Cliente)), SEEK_SET);
+        salva_cliente(c, arqDados);
+        free(aux);
         free(c);
         fclose(arqDados);
-        fclose(tab_hash);
-        return pLivre;
+        return livre;
     }
-
-     end = comp->prox;
-    fclose(tab_hash);
-    free(comp);
-    while (1)
-    {
-        fseek(arqDados, (end * sizeof(Cliente)), SEEK_SET);
-        Cliente *c = le_cliente(arqDados);
-        if (c->status == LIBERADO)
-        {
-            int prox = c->prox;
-            free(c);
-            c = cliente(cod_cli, nome_cli, prox, OCUPADO);
-            fseek(arqDados, (end * sizeof(Cliente)), SEEK_SET);
-            salva_cliente(c, arqDados);
-            free(c);
-            fclose(arqDados);
-            return end;
-        }
-        else if (c->prox == -1)
-        {
-            c->prox = pLivre;
-            fseek(arqDados, (end * sizeof(Cliente)), SEEK_SET);
-            salva_cliente(c, arqDados);
-            free(c);
-            c = cliente(cod_cli, nome_cli, -1, OCUPADO);
-            fseek(arqDados, (pLivre * sizeof(Cliente)), SEEK_SET);
-            salva_cliente(c, arqDados);
-            free(c);
-            fclose(arqDados);
-            return pLivre;
-        }
-        end = c->prox;
-    }
+  
 }
 
 int exclui(int cod_cli, char *nome_arquivo_hash, char *nome_arquivo_dados)
